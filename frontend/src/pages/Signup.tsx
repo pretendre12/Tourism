4b59f8c217799badb333728873e2408bf35f23b2
nishimiya-11 <quibra.jeranch11@gmail.com>
2025-05-22 03:46:15 +0000

    test2

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,7 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-import { useAuth } from '../context/AuthContext';
 import api from '../api';
 
 interface SignupFormValues {
